9ba2c649a3782f637e6314d0a3df61609a459204
Ignatov Dan <ignatovdan@gmail.com>
2023-01-01 21:13:11 +0300

    Update comments

--- stylelint-plugins/class-name-equal-to-file-name.js
+++ stylelint-plugins/class-name-equal-to-file-name.js
@@ -20,18 +20,6 @@ const messages = ruleMessages(ruleName, {
 
 const ruleFunction = () => {
     return (root, result) => {
-        //console.log(ruleName);
-
-        // eachDeclarationBlock
-        // https://github.com/stylelint/stylelint/blob/main/lib/rules/declaration-block-no-duplicate-properties/index.js
-        // https://github.com/stylelint/stylelint/tree/main/lib/utils
-        // https://github.com/search?q=stylelint+walkDecls&type=code
-        // https://github.com/nskazki/stylelint-selector-custom-tag-pattern/blob/master/index.js
-        // https://github.com/search?p=1&q=stylelint+%22walkTags%22&type=Code
-        // https://github.com/stylelint/stylelint/blob/main/lib/rules/selector-type-no-unknown/index.js
-        // https://postcss.org/api/  -- AtRule#walkDecls()
-        // https://postcss.org/api/#root
-
         const cssFullFilePath = root.source?.input?.file;
         const fileName = path.parse(cssFullFilePath).name;
         const fileBase = path.parse(cssFullFilePath).base;
@@ -54,7 +42,7 @@ const ruleFunction = () => {
                 // hasInterpolatingAmpersand(selector) - not supported yet
 
                 parseSelector(selector, result, rule, (selectorTree) => {
-                    // https://github.com/postcss/postcss-selector-parser/blob/master/API.md
+                    // walkClasses - see https://github.com/postcss/postcss-selector-parser/blob/master/API.md
                     selectorTree.walkClasses((classNode) => {
                         const cssClassName = classNode.value;
                         if (cssClassName !== fileName) {
