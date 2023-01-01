7ebda38863e59a596db1abe1bc7a6081748cdc0a
Ignatov Dan <ignatovdan@gmail.com>
2023-01-01 21:17:11 +0300

    Enable class file name rule (#7)
    * Enable class-name-equal-to-file-name
    
    * Update comments

--- stylelint-plugins/bem-newbie.js
+++ stylelint-plugins/bem-newbie.js
@@ -2,12 +2,15 @@ const validateImportFonts = require('./import-fonts.js');
 const validateImportNormalize = require('./import-normalize.js');
 const fontFaceDeclarationInFontsFileOnly = require('./font-face-declaration-in-fonts-file-only.js');
 const validateFontFaceDeclarationSrc = require('./font-face-duplicate-src');
+const classNameEqualToFileName = require('./class-name-equal-to-file-name');
+
 
 const plugins = [
   validateImportFonts,
   validateImportNormalize,
   fontFaceDeclarationInFontsFileOnly,
-  validateFontFaceDeclarationSrc
+  validateFontFaceDeclarationSrc, 
+  classNameEqualToFileName
 ];
 
 module.exports = plugins;
