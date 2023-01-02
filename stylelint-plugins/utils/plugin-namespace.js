3956bfc931f9cdd963a811cc0e6b94960036347a
Ignatov Dan <ignatovdan@gmail.com>
2023-01-03 02:16:33 +0300

    Use stylelint-bem-newbie package

--- stylelint-plugins/utils/plugin-namespace.js
+++
@@ -1,5 +0,0 @@
-const pluginNamespace = 'bem-newbie';
-
-module.exports = {
-  pluginNamespace
-};
--- stylelint-plugins/utils/unknownErrorOccurredRuleMessage.js
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore next */
-function unknownErrorOccurredRuleMessage(errorMessage) {
-  /* istanbul ignore next */
-  return `Unknown error occurred: '${errorMessage}'`;
-}
-
-module.exports = {
-  unknownErrorOccurredRuleMessage
-};
